33b718826adb27820942a6c83106e7f3d00f88d4
Francis Starr <38301914+francisstarr@users.noreply.github.com>
2020-03-03 15:34:29 -0500

    User Login Complete
    I created two controllers (Login, Home) each with one view each (Index.cshtml). The user logs in from the Login/Index.cshtml page then gets redirected to the Home/Index.cshtml page.  I also changed the routing to default to Login/Index.cshtml

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jindo_Capstone.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (Session["userName"] == null)
+            {
+                return RedirectToAction("../Login/Index");
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session["userName"] = null;
+            return Index();
+
+        }
+    }
+}
