4b48e7c4f073801671ac699acb902c54a3fec6b8
Brian Chiu <brianchiu33@gmail.com>
2020-03-07 12:42:13 -0500

    [Brian] - Added Customer Model and DBContext

---
+++ Model/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Jindo_Capstone.Model
+{
+    public class Customer
+    {
+        [Required]
+        public int CustID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        public DateTime NextReminderTime { get; set; }
+        public DateTime LastMessageDate { get; set; }
+        
+        [Required]
+        public bool IsSubscribed { get; set; }
+    }
+}
---
+++ Model/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Jindo_Capstone.Model
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
