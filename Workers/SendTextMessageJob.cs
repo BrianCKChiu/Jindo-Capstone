f3120eb1b7bb753079f53f97743a9658fc194ee6
Brian <brianchiu33@gmail.com>
2020-03-08 15:26:32 -0400

    [Brian] - added Twilio client and fixed minor errors

--- Workers/SendTextMessageJob.cs
+++ Workers/SendTextMessageJob.cs
@@ -4,31 +4,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using Jindo_Capstone.Clients;
 
 namespace Jindo_Capstone.Workers
 {
     public class SendTextMessageJob
     {
 
-        const string TxtMsgTempalte = "Hello {1}. This is an automated text message from CPOS. Would you like us to send a box of paper rolls? Type YES if you want to start the ordering process.";
+        const string TxtMsgTempalte = "Hello {0}. This is an automated text message from CPOS. Would you like us to send a box of paper rolls? Type YES if you want to start the ordering process.";
 
         public void Execute()
         {
             SubscribedCustomer().ForEach(c =>
             {
+                var twilioRestClient = new TwilioClient();
                 String message = String.Format(TxtMsgTempalte, c.Name);
                 using(DBContext db = new DBContext())
                 {
                     Message msgObject = new Message()
                     {
                         Customer = c,
-                        MessageContent = message
+                        MessageContent = message,
+                        Date = DateTime.Now
                     };
-                    
+                    twilioRestClient.SendMessage(msgObject);
                     db.Messages.Add(msgObject);
                 }
-                Console.WriteLine(c.Name);
             });
         }
 
